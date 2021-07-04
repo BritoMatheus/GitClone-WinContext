98c438302d612fd909c3ee9e5bad072639b14585
Matheus Brito <maatheusbrito@hotmail.com>
2021-07-03 21:08:24 -0300

    Add project files.

---
+++ GitCloneWinContext/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace GitCloneWinContext
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ConfigureTerminal();
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            try
+            {
+
+                var clipboard = Clipboard.GetText();
+                if (!ValidClipboard(clipboard))
+                    return;
+                Clone(clipboard, currentDirectory);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error => {ex.Message}");
+                Console.ReadLine();
+            }
+        }
+
+        internal static void ConfigureTerminal()
+        {
+            Console.Title = "GIT | WINCONTEXT";
+        }
+
+        internal static bool ValidClipboard(string clipboard)
+        {
+            if (!clipboard.EndsWith(".git") || (!clipboard.StartsWith("https") && !clipboard.StartsWith("http")))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("     ");
+                Console.WriteLine("     ");
+                Console.WriteLine("     ");
+                Console.WriteLine("     ");
+                Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "SEM REPOSITÓRIO PARA CLONAR!!!"));
+                Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+        internal static void Clone(string url, string directory)
+        {
+            new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"git",
+                    Arguments = $@"clone {url}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = $@"{directory}\"
+                }
+            }.Start();
+        }
+    }
+}
---
+++ GitCloneWinContextInstaller/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GitCloneWinContextInstaller
+{
+    class Program
+    {
+        //DesktopBackground
+        //Directory
+        //*
+        private const string Exe = @"\GitCloneWinContext.exe";
+        private const string MenuName = "\\shell\\GitCloneOption";
+        private const string Command = "\\shell\\GitCloneOption\\command";
+        private const string MenuLabel = "Git Clone";
+
+        private const string Title = "GIT | WinContextInstaller";
+
+        static void Main(string[] args)
+        {
+            Console.Title = Title;
+
+            try
+            {
+                AddRegistry("*");
+                AddRegistry("Directory");
+                AddRegistry(@"Directory\Background");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        internal static void AddRegistry(string root)
+        {
+            RegistryKey regmenu = null;
+            RegistryKey regcmd = null;
+            try
+            {
+                var currentDirectory = Directory.GetCurrentDirectory();
+
+                regmenu = Registry.ClassesRoot.CreateSubKey($"{root}{MenuName}");
+                if (regmenu != null)
+                {
+                    regmenu.SetValue("", MenuLabel);
+                    regmenu.SetValue("Icon", $@"{currentDirectory}\{Exe}");
+
+                }
+                regcmd = Registry.ClassesRoot.CreateSubKey($"{root}{Command}");
+                if (regcmd != null)
+                {
+                    regcmd.SetValue("", $@"{currentDirectory}\{Exe}");
+                }
+                MessageBox.Show("Add with success", Title);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("Execute as adm", Title, MessageBoxButtons.OK);
+                Close(regmenu, regcmd);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro: {ex.ToString()}", Title);
+                Close(regmenu, regcmd);
+                throw;
+            }
+            finally
+            {
+                Close(regmenu, regcmd);
+            }
+        }
+
+        internal static void Close(RegistryKey regmenu, RegistryKey regcmd)
+        {
+            if (regmenu != null)
+                regmenu.Close();
+            if (regcmd != null)
+                regcmd.Close();
+        }
+    }
+}
